13ab4d6aaa4ae7d98248157a0350e33bd9f279a2
Sam B <s77broz@aol.com>
2018-10-17 02:20:04 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,21 +3,12 @@ ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   ret = 0
   
-  out = arr.sort do |a, b|
-    i = 0
-    while i < a.length && i < b.length
-      ret = ESPERANTO_ALPHABET.index(a[i]) <=> ESPERANTO_ALPHABET.index(b[i])
-      
-      break unless i == 0
-      
-      i += 1
+  out = arr.sort_by do |word|
+    arr = []
+    word.each_char do |l|
+      arr << ESPERANTO_ALPHABET.index(l)
     end
-    
-    if a.length != b.length && ret == 0
-      ret = a.length <=> b.length
-    end
-    
-    ret
+    arr
   end
   
   out
