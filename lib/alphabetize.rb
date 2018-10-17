84e4c236e5a6a3cd3d5821a5431df8b3d3bb2890
Sam B <s77broz@aol.com>
2018-10-17 02:10:56 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,7 @@ def alphabetize(arr)
   ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   
   arr.sort_by do |a, b|
-    ret = 0
+    ret = 
     
     i = 0
     while i < a.length && i < b.length
